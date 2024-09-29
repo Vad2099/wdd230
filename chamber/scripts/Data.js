4662cdd95dcc9b533ce1438dd110ff874a3f792a
Vad2099 <valdemarlasnibat@hotmail.es>
2024-09-29 16:32:29 -0600

    GAM chamber of commerce first addition

---
+++ chamber/scripts/Data.js
@@ -0,0 +1,5 @@
+// Dynamically populate the current year in the footer
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Dynamically populate the last modified date
+document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
